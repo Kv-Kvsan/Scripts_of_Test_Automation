d49336e8bf7ddcb744a13f003af27361508e6c19
Jagan <Jagan@DESKTOP-J7791LE>
2025-07-14 17:51:38 +0530

    Commit the changes

---
+++ src/test/java/Runner/Runner_Class_Execute_At_Once.java
@@ -0,0 +1,17 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Feature_Files/", glue = {"stepDefinitions"}, plugin = {
+        "pretty",
+        "html:target/cucumber-reports/report.html",
+        "json:target/cucumber-reports/report.json",
+        "junit:target/cucumber-reports/report.xml"
+        })
+public class Runner_Class_Execute_At_Once {
+
+}
