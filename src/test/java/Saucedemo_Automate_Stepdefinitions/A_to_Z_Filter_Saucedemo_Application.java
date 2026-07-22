e1897b10ff465bc9d514d44ee5e690170d771f58
Jagan <Jagan@DESKTOP-J7791LE>
2026-07-22 16:17:04 +0530

    Commit the changes

--- src/test/java/Saucedemo_Automate_Stepdefinitions/A_to_Z_Filter_Saucedemo_Application.java
+++ src/test/java/Saucedemo_Automate_Stepdefinitions/A_to_Z_Filter_Saucedemo_Application.java
@@ -18,23 +18,38 @@ public class A_to_Z_Filter_Saucedemo_Application {
 	@Then("the user retrieves the product names in alphabetical order and confirm whether it is changed or not")
 	public void the_user_retrieves_the_product_names_in_alphabetical_order_and_confirm_whether_it_is_changed_or_not()
 	{
-		
+
 		driver=Hooks_Saucedemo_Automate.driver;
 		List<WebElement> Product_Names=driver.findElements(By.xpath("//div[@class='inventory_item_name ']"));
+		
+		List<WebElement> Default_Prices=driver.findElements(By.xpath("//div[@class='inventory_item_price']"));
 
 		List<String> actualNames = new ArrayList<>();
+
+
+		List<Double> actualPrices=new  ArrayList<>();
 
 		for(WebElement Names: Product_Names)
 		{
 			actualNames.add(Names.getText());
-			System.out.println(Names.getText()); 
-
+			System.out.println(Names.getText());
 		}
+		
+			for(WebElement prices: Default_Prices)
+			{
+				String value = prices.getText().replace("$", "");
+				actualPrices.add(Double.parseDouble(value));
+				System.out.println(value); 
+			}
+
 
 		List<String> expectedNames = new ArrayList<>(actualNames);
+		List<Double> expectedPrices=new  ArrayList<>(actualPrices);
 		Collections.sort(expectedNames);
+		
 
 		Assert.assertEquals(expectedNames, actualNames);
+		Assert.assertEquals(expectedPrices, actualPrices);
 
 	}
 }
